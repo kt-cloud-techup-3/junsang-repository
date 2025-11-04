2b422c4eb478a179f1041ec6b8dbbac7259f3faf
junsang-ee <dkwnstkddk@gmail.com>
2025-11-04 15:08:53 +0900

    feat: user create 로직 및 API 생성 (UserController, service repository, createRequestDto 생성)

---
+++ src/main/java/com/kt/shopping/service/UserServiceImpl.java
@@ -0,0 +1,38 @@
+package com.kt.shopping.service;
+
+import com.kt.shopping.domain.User;
+import com.kt.shopping.domain.dto.request.UserCreateRequest;
+import com.kt.shopping.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public void create(UserCreateRequest request) {
+        System.out.println(request.toString());
+        var newUser = new User(
+                userRepository.selectMaxId() + 1,
+                request.loginId(),
+                request.password(),
+                request.name(),
+                request.email(),
+                request.mobile(),
+                request.gender(),
+                request.birthday(),
+                LocalDateTime.now(),
+                LocalDateTime.now()
+        );
+
+        userRepository.save(newUser);
+    }
+
+}
