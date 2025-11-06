cd80e9c435ceeb8a586ca36edfd732e75ab0a41a
junsang-ee <dkwnstkddk@gmail.com>
2025-11-06 15:18:09 +0900

    feat: 기존 UserRepository -> UserJDBCRepository, UserRepository 생성 (수정작업으로 인지 JPA Repository interface 생성)

--- src/main/java/com/kt/shopping/service/UserServiceImpl.java
+++ src/main/java/com/kt/shopping/service/UserServiceImpl.java
@@ -3,7 +3,7 @@ package com.kt.shopping.service;
 import com.kt.shopping.domain.User;
 import com.kt.shopping.domain.dto.request.UserCreateRequest;
 import com.kt.shopping.domain.dto.response.CustomPage;
-import com.kt.shopping.repository.UserRepository;
+import com.kt.shopping.repository.UserJDBCRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,14 +14,14 @@ import java.time.LocalDateTime;
 @Service
 public class UserServiceImpl implements UserService {
 
-    private final UserRepository userRepository;
+    private final UserJDBCRepository userJDBCRepository;
 
     @Override
     @Transactional
     public void create(UserCreateRequest request) {
         System.out.println(request.toString());
         User user = new User(
-                userRepository.selectMaxId() + 1,
+                userJDBCRepository.selectMaxId() + 1,
                 request.loginId(),
                 request.password(),
                 request.name(),
@@ -33,30 +33,30 @@ public class UserServiceImpl implements UserService {
                 LocalDateTime.now()
         );
 
-        userRepository.save(user);
+        userJDBCRepository.save(user);
     }
 
     @Override
     public boolean isDuplicateLoginId(String loginId) {
-        return userRepository.existsByLoginId(loginId);
+        return userJDBCRepository.existsByLoginId(loginId);
     }
 
     @Override
     @Transactional
     public void changePassword(int id, String oldPassword, String password) {
-        var user = userRepository.selectById(id)
+        var user = userJDBCRepository.selectById(id)
                 .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 회원입니다."));
 
         if(!user.getPassword().equals(oldPassword)) {
             throw new IllegalArgumentException("기존 비밀번호가 일치하지 않습니다.");
         }
 
-        userRepository.updatePassword(id, password);
+        userJDBCRepository.updatePassword(id, password);
     }
 
     @Override
     public User detail(Long id) {
-        return userRepository.selectById(id).orElseThrow(
+        return userJDBCRepository.selectById(id).orElseThrow(
                 () -> new IllegalArgumentException("존재하지 않는 회원입니다.")
         );
     }
@@ -64,20 +64,20 @@ public class UserServiceImpl implements UserService {
     @Override
     @Transactional
     public void update(Long id, String name, String email, String mobile) {
-        userRepository.selectById(id)
+        userJDBCRepository.selectById(id)
                 .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 회원입니다."));
 
-        userRepository.updateById(id, name, email, mobile);
+        userJDBCRepository.updateById(id, name, email, mobile);
     }
 
     @Override
     @Transactional
     public void delete(Long id) {
-        userRepository.deleteById(id);
+        userJDBCRepository.deleteById(id);
     }
 
     public CustomPage search(int page, int size, String keyword) {
-        var pair = userRepository.selectAll(page - 1, size, keyword);
+        var pair = userJDBCRepository.selectAll(page - 1, size, keyword);
         var pages = (int) Math.ceil((double) pair.getSecond() / size);
 
         return new CustomPage(
