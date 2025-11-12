ab9e7cdd8b7d57bb172c06a85c48d6ecb10d6da3
junsang-ee <dkwnstkddk@gmail.com>
2025-11-12 14:09:02 +0900

    feat: payment 모델 생성

---
+++ src/main/java/com/kt/shopping/domain/model/payment/Payment.java
@@ -0,0 +1,27 @@
+package com.kt.shopping.domain.model.payment;
+
+import com.kt.shopping.constants.PaymentType;
+import com.kt.shopping.domain.model.common.BaseEntity;
+import com.kt.shopping.domain.model.order.Order;
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Getter
+@Entity
+public class Payment extends BaseEntity {
+
+    @Column(nullable = false)
+    private Long totalPrice;
+
+    @Column(nullable = false)
+    private Long deliveryFee;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private PaymentType type;
+
+    @OneToOne
+    @JoinColumn(nullable = false)
+    private Order order;
+
+}
